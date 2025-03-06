8190f7b965c4d05d161e6ebbe2f68e219a3af550
shashank-amireddy <amireddy.shashank31@gmail.com>
2025-03-06 17:53:41 +0530

    Add campaign management functions and wallet connection logic

--- Context/CrowdFunding.js
+++ Context/CrowdFunding.js
@@ -13,6 +13,14 @@ export const CrowdFundingProvider = ({children}) => {
     const titleData = "Crowd Funding Contract";
     const [currentAccount, setCurrentAccount] = useState("");
 
+    /**
+     * Create a new campaign.
+     * @param {Object} campaign - The campaign details.
+     * @param {string} campaign.title - The title of the campaign.
+     * @param {string} campaign.description - The description of the campaign.
+     * @param {string} campaign.amount - The target amount for the campaign.
+     * @param {string} campaign.deadline - The deadline for the campaign.
+     */
     const createCampaign = async (campaign) => {
         const {title, description, amount, deadline} = campaign;
         const web3Modal = new Wenb3Modal();
@@ -39,3 +47,163 @@ export const CrowdFundingProvider = ({children}) => {
         }
     };
     
+    /**
+     * Get all campaigns.
+     * @returns {Array} - An array of campaign objects.
+     */
+    const getCampaigns = async () => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);
+
+        const campaigns = await contract.getCampaigns();
+
+        const parsedCampaigns = campaigns.map((campaign) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            pId: i,
+        }));
+
+        return parsedCampaigns;
+    };
+
+    /**
+     * Get campaigns created by the current user.
+     * @returns {Array} - An array of campaign objects created by the current user.
+     */
+    const getuserCampaigns = async() => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);
+
+        const allCampoaigns = await contract.getCampaigns();
+        const accounts = await window.ethereum.request({method: "eth_accounts"});
+        const currentUser = accounts[0];
+
+        const filteredCampaigns = allCampoaigns.filter((campaign) => campaign.owner === "Oxf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+
+        const userData = filteredCampaigns.map((campaign,i) => ({
+            owner: campaign.owner,
+            title: campaign.title,
+            description: campaign.description,
+            target: ethers.utils.formatEther(campaign.target.toString()),
+            deadline: campaign.deadline.toNumber(),
+            amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+            pId: i,
+        }));
+
+        return userData;
+    };
+
+    /**
+     * Donate to a campaign.
+     * @param {string} pId - The ID of the campaign.
+     * @param {string} amount - The amount to donate.
+     * @returns {Object} - The transaction data.
+     */
+    const donate = async (pId, amount) => {
+        const web3Modal = new Wenb3Modal();
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = fetchContract(signer);
+
+        const campaignData = await contract.donateToCampaign(pId, {
+            value: ethers.utils.parseUnits(amount),
+        });
+
+        await campaignData.wait();
+        location.reload();
+
+        return campaignData;
+    };
+
+    /**
+     * Get donations for a campaign.
+     * @param {string} pId - The ID of the campaign.
+     * @returns {Array} - An array of donation objects.
+     */
+    const getDonations = async (pId) => {
+        const provider = new ethers.providers.JsonRpcProvider();
+        const contract = fetchContract(provider);
+
+        const donations = await contract.getDonations(pId);
+        const numberOfDonations = donations[0].length;
+
+        const parsedDonations = [];
+
+        for(let i =0; i< numberOfDonations; i++){
+            parsedDonations.push({
+                donor: donations[0][i],
+                amount: ethers.utils.formatEther(donations[1][i].toString()),
+            });
+        }
+
+        return parsedDonations;
+    }
+
+    /**
+     * Check if the wallet is connected.
+     * @returns {boolean} - True if the wallet is connected, false otherwise.
+     */
+    const checkIfWalletIsConnected = async () => {
+        try{
+            if(!window.ethereum){
+                return setOpenError(true), setError("Please install MetaMask");
+            }
+
+            const acounts = await window.ethereum.request({
+                method: "eth_accounts",
+            });
+
+            if(accounts.length){
+                setCurrentAccount(accounts[0]);
+                return true;
+            } else {
+                console.log("Please connect your wallet");
+            }
+        } catch (error){
+            console.log("Error connecting wallet", error);
+        }
+    };
+
+    useEffect(() => {
+        checkIfWalletIsConnected();
+    }, []);
+
+    /**
+     * Connect the wallet.
+     */
+    const connectWallet = async () => {
+        try{
+            if(!window.ethereum){
+                return setOpenError(true), setError("Please install MetaMask");
+            }
+
+            const accounts = await window.ethereum.request({
+                method: "eth_requestAccounts",
+            });
+
+            setCurrentAccount(accounts[0]);
+        } catch (error){
+            console.log("Error connecting wallet", error);
+        }
+    };
+
+    return (
+        <CrowdFundingContext.Provider value={{
+            titleData,
+            currentAccount,
+            createCampaign,
+            getCampaigns,
+            getuserCampaigns,
+            donate,
+            getDonations,
+            connectWallet,
+        }}>
+            {children}
+        </CrowdFundingContext.Provider>
+    );
+};
